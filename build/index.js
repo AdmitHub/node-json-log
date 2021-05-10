a7f237e0d8cdf5b29dbc527ff2aaa8ae93e8c91e
UrosNikolic <unikolic94@gmail.com>
2021-05-10 14:31:50 +0200

    move types to dev dependency add build folder

---
+++ build/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var logger_1 = require("./lib/logger");
+var raven_initializer_1 = require("./lib/raven-initializer");
+module.exports = {
+    log: logger_1.createLogger(),
+    createLogger: logger_1.createLogger,
+    installLogger: function (config) {
+        raven_initializer_1.installRaven(config);
+    }
+};
